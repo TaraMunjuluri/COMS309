e6c62616a6f0535a506e127216e9cbe0bfd401f4
AeliaRiz123 <aeliariz123@gmail.com>
2024-09-10 16:28:11 -0500

    Added new endpoints to controller

--- Experiments/arizvi/tutorials-springboot_unit1_2_hellopeople/tutorials-springboot_unit1_2_hellopeople/hellopeople/src/main/java/coms309/ExceptionController.java
+++ Experiments/arizvi/tutorials-springboot_unit1_2_hellopeople/tutorials-springboot_unit1_2_hellopeople/hellopeople/src/main/java/coms309/ExceptionController.java
@@ -15,7 +15,7 @@ class ExceptionController {
 
     @RequestMapping(method = RequestMethod.GET, path = "/oops")
     public String triggerException() {
-        throw new RuntimeException("Check to see what happens when an exception is thrown");
+        throw new RuntimeException("Oops, there's a problem! Sorry buddy");
     }
 
 }
--- Experiments/arizvi/tutorials-springboot_unit1_2_hellopeople/tutorials-springboot_unit1_2_hellopeople/hellopeople/src/main/java/coms309/WelcomeController.java
+++ Experiments/arizvi/tutorials-springboot_unit1_2_hellopeople/tutorials-springboot_unit1_2_hellopeople/hellopeople/src/main/java/coms309/WelcomeController.java
@@ -3,6 +3,7 @@ package coms309;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 /**
  * Simple Hello World Controller to display the string returned
  *
@@ -14,6 +15,13 @@ class WelcomeController {
 
     @GetMapping("/")
     public String welcome() {
-        return "Hello and welcome to COMS 309";
+        return "Hey what's up, it's Aelia coming at you";
     }
+
+    @GetMapping("/greeting")
+    public String greeting() {return "Greetings from Aelia!"; }
+
+    @GetMapping("/bye")
+    public String bye() {return "Good Bye, Good Morrow! Have a wonderful day!";}
+
 }
