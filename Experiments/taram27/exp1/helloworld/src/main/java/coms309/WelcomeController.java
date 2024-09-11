0fe418bef4568a2a076e3822b62a9981c2f59b7e
TaraMunjuluri <tara.munjuluri@gmail.com>
2024-09-11 12:28:00 -0500

    Added changes to Exp1

--- Experiments/taram27/exp1/helloworld/src/main/java/coms309/WelcomeController.java
+++ Experiments/taram27/exp1/helloworld/src/main/java/coms309/WelcomeController.java
@@ -9,11 +9,18 @@ class WelcomeController {
 
     @GetMapping("/")
     public String welcome() {
-        return "Hello and welcome to COMS 309";
+        return "This is the welcome page";
     }
-
+    @GetMapping("/Home")
+    public String Home() {
+        return "Hello, this is home";
+    }
+    @GetMapping("/Contact")
+    public String Contact() {
+        return "Hello, this is the contact page";
+    }
     @GetMapping("/{name}")
     public String welcome(@PathVariable String name) {
-        return "Hello and welcome to COMS 309: " + name;
+        return "Hello and welcome:Q " + name;
     }
 }
