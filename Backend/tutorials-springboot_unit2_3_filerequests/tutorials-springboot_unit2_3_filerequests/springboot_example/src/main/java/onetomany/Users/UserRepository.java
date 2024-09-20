fb71f780b2c4a31c84f8c931f35273e77dba8d8b
AeliaRiz123 <aeliariz123@gmail.com>
2024-09-19 23:51:59 -0500

    added user and phone controller

--- Backend/tutorials-springboot_unit2_3_filerequests/tutorials-springboot_unit2_3_filerequests/springboot_example/src/main/java/onetomany/Users/UserRepository.java
+++ Backend/tutorials-springboot_unit2_3_filerequests/tutorials-springboot_unit2_3_filerequests/springboot_example/src/main/java/onetomany/Users/UserRepository.java
@@ -1,7 +1,6 @@
-package onetoone.Users;
+package onetomany.Users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 
@@ -10,10 +9,6 @@ import org.springframework.transaction.annotation.Transactional;
  */ 
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    
     User findById(int id);
-
     void deleteById(int id);
-
-    User findByLaptop_Id(int id);
 }
