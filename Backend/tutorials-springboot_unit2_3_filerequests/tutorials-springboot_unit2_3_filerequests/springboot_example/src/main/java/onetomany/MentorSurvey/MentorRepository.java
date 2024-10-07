5fc1e90fe63d42cac349f7e7812d6cf5d6f9dc3e
TaraMunjuluri <tara.munjuluri@gmail.com>
2024-10-06 23:03:13 -0500

    repository

---
+++ Backend/tutorials-springboot_unit2_3_filerequests/tutorials-springboot_unit2_3_filerequests/springboot_example/src/main/java/onetomany/MentorSurvey/MentorRepository.java
@@ -0,0 +1,7 @@
+package onetomany.MentorSurvey;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MentorRepository extends JpaRepository<Mentor, Long> {
+    Mentor findByUserId(int userId);
+}
