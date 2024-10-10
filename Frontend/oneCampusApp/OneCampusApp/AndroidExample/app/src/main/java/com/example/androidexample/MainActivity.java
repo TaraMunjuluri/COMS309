e542a23008bdb5d776b23c5f5e8776cec164b9da
Mariam-Rizvi <mariam.riz123@gmail.com>
2024-10-10 11:41:41 -0500

    got rid of username text, centered main page...I hope

--- Frontend/oneCampusApp/OneCampusApp/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Frontend/oneCampusApp/OneCampusApp/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -29,21 +29,21 @@ public class MainActivity extends AppCompatActivity {
 
         /* initialize UI elements */
 //        messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
-        usernameText = findViewById(R.id.main_username_txt);// link to username textview in the Main activity XML
+//        usernameText = findViewById(R.id.main_username_txt);// link to username textview in the Main activity XML
         loginButton = findViewById(R.id.main_login_btn);    // link to login button in the Main activity XML
         signupButton = findViewById(R.id.main_signup_btn);  // link to signup button in the Main activity XML
 
         /* extract data passed into this activity from another activity */
         Bundle extras = getIntent().getExtras();
-        if(extras == null) {
+//        if(extras == null) {
 //            messageText.setText("Welcome"); // Apply the text with stroke
-            usernameText.setVisibility(View.INVISIBLE);             // set username text invisible initially
-        } else {
+//            usernameText.setVisibility(View.INVISIBLE);             // set username text invisible initially
+//        } else {
 //            messageText.setText("Welcome");
-            usernameText.setText(extras.getString("USERNAME")); // this will come from LoginActivity
-            loginButton.setVisibility(View.INVISIBLE);              // set login button invisible
-            signupButton.setVisibility(View.INVISIBLE);             // set signup button invisible
-        }
+//            usernameText.setText(extras.getString("USERNAME")); // this will come from LoginActivity
+//            loginButton.setVisibility(View.INVISIBLE);              // set login button invisible
+//            signupButton.setVisibility(View.INVISIBLE);             // set signup button invisible
+//        }
 
         /* click listener on login button pressed */
         loginButton.setOnClickListener(new View.OnClickListener() {
