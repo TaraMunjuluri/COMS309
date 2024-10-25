5d3cedec389095a6697d7f7c99239beeb9450b24
AeliaRiz123 <aeliariz123@gmail.com>
2024-10-24 21:42:18 -0500

    added code for message table and fixed jakarta errors

--- Backend/tutorials-springboot_unit2_3_filerequests/tutorials-springboot_unit2_3_filerequests/springboot_example/src/main/java/onetomany/Chatbox/ChatboxRepository.java
+++
@@ -1,4 +0,0 @@
-package onetomany.Chatbox;
-
-public interface ChatboxRepository {
-}
--- Backend/tutorials-springboot_unit2_3_filerequests/tutorials-springboot_unit2_3_filerequests/springboot_example/src/main/java/onetomany/Users/User.java
+++ Backend/tutorials-springboot_unit2_3_filerequests/tutorials-springboot_unit2_3_filerequests/springboot_example/src/main/java/onetomany/Users/User.java
@@ -346,7 +346,6 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import onetomany.Chatbox.Message;
 @Entity
 @Table(name = "users")
 public class User {
@@ -376,9 +375,6 @@ public class User {
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "phone_id"))
     private List<Phone> phones = new ArrayList<>();
-
-    @OneToMany(mappedBy = "sender", cascade = CascadeType.ALL)
-    private List<Message> messages;
 
     // Constructors
     public User(String name, String emailId, Date joiningDate) {
