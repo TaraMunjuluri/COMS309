eb9e16084204e4ce0c34fcbf006b6cb3939d77f2
TaraMunjuluri <tara.munjuluri@gmail.com>
2024-11-08 04:26:52 -0600

    added back the functions removed from websocket.js

--- Backend/tutorials-springboot_unit2_3_filerequests/tutorials-springboot_unit2_3_filerequests/springboot_example/src/main/java/onetomany/Client_JS/websocket.js
+++ Backend/tutorials-springboot_unit2_3_filerequests/tutorials-springboot_unit2_3_filerequests/springboot_example/src/main/java/onetomany/Client_JS/websocket.js
@@ -1,33 +1,62 @@
 var ws;
+let typingTimeout;
+const typingInterval = 1000; // 1 second delay for "TYPING_STOP"
 
+// Function to connect to the WebSocket server
 function connect() {
     var username = document.getElementById("username").value;
     var wsserver = document.getElementById("wsserver").value;
     var url = wsserver + username;
-    //var url = "ws://echo.websocket.org";
 
     ws = new WebSocket(url);
 
     ws.onmessage = function(event) { // Called when client receives a message from the server
-        console.log(event.data);
+        console.log("Message from server:", event.data);
 
-        // display on browser
+        // Display on browser
         var log = document.getElementById("log");
         log.innerHTML += "message from server: " + event.data + "\n";
     };
 
-
-
-    ws.onopen = function(event) { // called when connection is opened
+    ws.onopen = function(event) { // Called when connection is opened
         var log = document.getElementById("log");
         log.innerHTML += "Connected to " + event.currentTarget.url + "\n";
     };
 }
 
-//function send() {  // this is how to send messages
-//    var content = document.getElementById("msg").value;
-//    ws.send(content);
-//}
+// Send typing notification
+function sendTypingNotification(isTyping) {
+    if (isTyping) {
+        console.log("Sending TYPING_START");
+        ws.send("TYPING_START");
+    } else {
+        console.log("Sending TYPING_STOP");
+        ws.send("TYPING_STOP");
+    }
+}
+
+// Function to send messages
+function send() {
+    var content = document.getElementById("msg").value;
+    if (content.trim() !== "") {
+        console.log("Sending message:", content);
+        ws.send(content);
+        document.getElementById("msg").value = ""; // Clear input
+        sendTypingNotification(false); // Stop typing notification when a message is sent
+    }
+}
+
+// Detect typing in the message input field
+document.getElementById("msg").addEventListener("input", () => {
+    clearTimeout(typingTimeout);
+    sendTypingNotification(true); // Notify typing started
+
+    typingTimeout = setTimeout(() => {
+        sendTypingNotification(false); // Notify typing stopped after delay
+    }, typingInterval);
+});
+
+
 @Controller
 public class MatchNotificationController {
 
