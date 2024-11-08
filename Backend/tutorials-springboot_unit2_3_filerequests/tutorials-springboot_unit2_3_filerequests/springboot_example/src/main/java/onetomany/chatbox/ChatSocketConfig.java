2381b1a0d980001bf2aaf3d5763e77cffdecd4e3
TaraMunjuluri <tara.munjuluri@gmail.com>
2024-11-08 06:44:15 -0600

    mismatched typed: now fixed

--- Backend/tutorials-springboot_unit2_3_filerequests/tutorials-springboot_unit2_3_filerequests/springboot_example/src/main/java/onetomany/chatbox/ChatSocketConfig.java
+++ Backend/tutorials-springboot_unit2_3_filerequests/tutorials-springboot_unit2_3_filerequests/springboot_example/src/main/java/onetomany/chatbox/ChatSocketConfig.java
@@ -1,16 +1,16 @@
-//package onetomany.chatbox;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.socket.server.standard.ServerEndpointExporter;
-//
-//
-//@Configuration
-//public class ChatSocketConfig {
-//    @Bean
-//    public ServerEndpointExporter serverEndpointExporter() {
-//        return new ServerEndpointExporter();
-//    }
-//
-//
-//}
+package onetomany.chatbox;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+
+@Configuration
+public class ChatSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
+
+}
--- Backend/tutorials-springboot_unit2_3_filerequests/tutorials-springboot_unit2_3_filerequests/springboot_example/src/main/java/onetomany/chatbox/MessageRepository.java
+++ Backend/tutorials-springboot_unit2_3_filerequests/tutorials-springboot_unit2_3_filerequests/springboot_example/src/main/java/onetomany/chatbox/MessageRepository.java
@@ -1,5 +1,5 @@
-//package onetomany.chatbox;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//public interface MessageRepository extends JpaRepository<Message, Long>{
-//}
+package onetomany.chatbox;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface MessageRepository extends JpaRepository<Message, Long>{
+}
