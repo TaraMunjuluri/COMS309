3a18e13f011c07ee23d49f59e02b4110786de52b
TaraMunjuluri <tara.munjuluri@gmail.com>
2024-11-08 23:41:59 -0600

    added pitential friends you can find based on interest

--- Backend/tutorials-springboot_unit2_3_filerequests/tutorials-springboot_unit2_3_filerequests/springboot_example/src/main/java/onetomany/PotentialFriends/PotentialFriendsRepository.java
+++ Backend/tutorials-springboot_unit2_3_filerequests/tutorials-springboot_unit2_3_filerequests/springboot_example/src/main/java/onetomany/PotentialFriends/PotentialFriendsRepository.java
@@ -1,11 +1,11 @@
-package onetomany.PotentialFriends;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import java.util.List;
-
-@Repository
-public interface PotentialFriendsRepository extends JpaRepository<PotentialFriends, Long> {
-    List<PotentialFriends> findByUserIdOrderByCommonInterestCountDesc(long userId);
-    void deleteByUserId(long userId);
-}
+//package onetomany.PotentialFriends;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//import java.util.List;
+//
+//@Repository
+//public interface PotentialFriendsRepository extends JpaRepository<PotentialFriends, Long> {
+//    List<PotentialFriends> findByUserIdOrderByCommonInterestCountDesc(long userId);
+//    void deleteByUserId(long userId);
+//}
